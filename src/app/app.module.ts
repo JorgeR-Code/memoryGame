bd72d296ce07e9ba1d7decfcc9768e8a36d95c14
Jorge De Jesus Rivera Perez <i20163mc061@utez.edu.mx>
2021-12-04 16:23:57 -0600

    Modules and components
    New modules and components for the project structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,9 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CardsModule } from './cards/cards.module';
 import { PrimeNgModule } from './prime-ng/prime-ng.module';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [
@@ -10,7 +12,9 @@ import { PrimeNgModule } from './prime-ng/prime-ng.module';
   ],
   imports: [
     BrowserModule,
-    PrimeNgModule
+    PrimeNgModule,
+    CardsModule,
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
