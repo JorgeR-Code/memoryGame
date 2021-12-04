8883a927dc43e73b2dc0e64a81f07ddf92c81ca2
Jorge De Jesus Rivera Perez <i20163mc061@utez.edu.mx>
2021-12-04 16:04:32 -0600

    First commit
    Installation and configuration of primeNG

---
+++ src/app/prime-ng/prime-ng.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+//PrimeNG
+import {ButtonModule} from 'primeng/button';
+
+
+@NgModule({
+  exports:[
+    ButtonModule
+  ]
+})
+export class PrimeNgModule { }
